d3dd598a529ae8cd6304f285e1bdb262a48d4eca
Guy Smith <guy@hma.co.uk>
2021-01-08 10:46:15 +0000

    Day6 more work

--- src/routes/Day6.svelte
+++ src/routes/Day6.svelte
@@ -12,44 +12,57 @@
     const { width, height } = canvas;
     const half = width/2;
 
-    const numLines = 20;
+    const numLines = 26;
     const spacer = (height - 60)/numLines;
+    
+    const numCurves = 12;
+    const xSpacer = width/numCurves;
 
-    const numCurves = 4;
-
-    let start = width * 0.2; // Start wave 0.2 across
-    let end = width * 0.8; // End with 0.2 to go
-    let dx = (end - start) / numCurves; // Distance across for each wave to go
+    // let start = width * 0.2; // Start wave 0.2 across
+    // let end = width * 0.8; // End with 0.2 to go
+    // let dx = (end - start) / numCurves; // Distance across for each wave to go
 
     function draw(t) {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.strokeStyle = 'black';
       ctx.lineWidth = 2;
 
+      const genCurve = (x, yPos) => {
+        // let xPos = start + (x*dx);
+          // let xPosEnd = start + ((x+1)*dx);
+          let xPos = (x * xSpacer);
+          let xPosEnd = ((x+1)*xSpacer);
+
+          // Modifier -> should be higher in the center. From 0 - 100
+          let dFromCenter = map(Math.abs(half - xPos), half, 0, 0, 100);
+
+          let yPosEnd = yPos - Math.random()*dFromCenter;
+
+          let c1x = (xPos + xPosEnd)/2;
+          let c1y = (yPos + yPosEnd)/2;
+          // ctx.lineTo(xPosEnd, yPosEnd);
+          return ctx.quadraticCurveTo(xPos, yPosEnd, c1x, c1y);
+          // ctx.quadraticCurveTo(c1x, c1y, xPosEnd, yPosEnd);
+      }
+
       for( let y = 0; y <= numLines; y++){
         let yPos = (y * spacer) + 50;
 
         ctx.beginPath();
         ctx.moveTo(0, yPos);
-        ctx.lineTo(start, yPos);
 
-        for (let x = 0; x <= numCurves; x++) {
-
-          let xPos = start + (x*dx);
-
-          // Modifier -> should be higher in the center. From 0 - 100
-          let dFromCenter = map(Math.abs(half - xPos), 120, 0, 0, 100);
-
-          let c1x = xPos;
-          let c1y = -spacer + yPos + (spacer * Math.random());
-          console.log(x, c1y);
-          let c2x = xPos;
-          let c2y = Math.random() * spacer + yPos - (spacer * Math.random());
-          let endX = xPos;
-          let endY = yPos - dFromCenter;
-          ctx.bezierCurveTo(c1x, c1y, c2x, c2y, endX, endY);
+        for (let x = 0; x <= numCurves-2; x++) {
+          genCurve(x, yPos)
         }
+
+        let penultimateX = numCurves - 1;
+
+        ctx.lineTo(penultimateX * xSpacer, yPos);
         ctx.lineTo(width, yPos);
+        ctx.save();
+        ctx.globalCompositeOperation = 'destination-out'; // Control composite to hide lines underneath
+        ctx.fill();
+        ctx.restore();
         ctx.stroke();
       }
     }
