07b8bc2c9e4dcd7ac2def46079fa5099c3408303
Guy <gcschino@hotmail.com>
2021-01-16 14:59:18 +0000

    Update Day17

--- src/routes/Day17.svelte
+++ src/routes/Day17.svelte
@@ -4,12 +4,12 @@
   import { map, dist } from '../utils';
   import SimplexNoise from 'simplex-noise';
 
-  const threshold = 62;
+  const threshold = 42;
 
   const sNoise = new SimplexNoise();
 
   class Particle {
-    constructor(x, y, speedX, speedY, rgb, size, angle = 0) {
+    constructor(x, y, speedX, speedY, rgb, angle = 0) {
       this.id = Math.floor(x + y);
       this.x = x;
       this.y = y;
@@ -28,16 +28,12 @@
     }
 
     move(canvasWidth, canvasHeight) {
-      this.x = this.x + this.speedX;
-      this.y = this.y + this.speedY;
+      this.x = this.x + this.speedX + (sNoise.noise2D(this.x, this.y) * 3);
+      this.y = this.y + this.speedY + (sNoise.noise2D(this.x, this.y) * 3);
 
       // Bounce off walls
       if (this.x >= canvasWidth || this.x <= 0) this.speedX *= -1;
       if (this.y >= canvasHeight || this.y <= 0) this.speedY *= -1;
-
-      // Change speed slightly based on noise map of canvas
-      this.speedX -= sNoise.noise2D(this.x, this.y) * 0.25;
-      this.speedY += sNoise.noise2D(this.x, this.y) * 0.25;
     }
 
     render(ctx, points) {
@@ -45,11 +41,10 @@
       points.forEach(p => {
         let distanceAway = dist(this.x, this.y, p.x, p.y);
         if (this.id !== p.id && distanceAway < threshold) {
-          ctx.strokeStyle = `rgba(${this.rgb}, ${map(distanceAway, threshold, 0, 0, 0.2)})`;
+          ctx.strokeStyle = `rgba(${this.rgb}, ${map(distanceAway, threshold, 0, 0, 0.05)})`;
           ctx.beginPath();
           ctx.lineTo(this.x, this.y);
           let diffX = p.x - this.x;
-          let diffY = p.y - this.y;
           ctx.lineTo(this.x + diffX, this.y);
           ctx.lineTo(p.x, p.y);
           ctx.stroke();
@@ -60,7 +55,7 @@
       ctx.beginPath();
       ctx.ellipse(this.x, this.y, this.size, this.size, 0, 0, Math.PI*2);
       ctx.closePath();
-      ctx.fillStyle = `rgb(${this.rgb})`;
+      ctx.fillStyle = `rgba(${this.rgb}, 0.2)`;
       ctx.fill();
     }
   }
@@ -73,19 +68,23 @@
     let frame = requestAnimationFrame(draw);
 
     const { width, height } = canvas;
-    const numParticles = 80;
+    const numParticles = 100;
 
 
     ctx.strokeStyle = 'rgba(0, 0, 0, 0.2)';
     ctx.fillStyle = 'rgba(0 0 0 1)';
 
-    // Create particles with random positions and speeds
+    // Create particles positioned around a circle
+    const r = width/4;
     const particles = [];
     for (let index = 0; index < numParticles; index++) {
-      particles[index] = new Particle(Math.random() * width, Math.random() * height, (Math.random() * 2) - 1, (Math.random() * 2) - 1, `${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255}`);
-    }
+      let theta = ((Math.PI*2) / numParticles);
+      let angle = (theta * index);
+      let rPosX = width/2 +  (r * Math.cos(angle));
+      let rPosY = height/2 + (r * Math.sin(angle));
 
-    // console.log(particles);
+      particles[index] = new Particle(rPosX, rPosY,  (Math.random() - 0.5) * 0.75, (Math.random() - 0.5) * 0.75, `${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255}`);
+    }
 
     function draw(t) {
       requestAnimationFrame(draw);
