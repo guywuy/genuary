022aadd419e39ce14dc432b8ef0e3c7177585684
Guy Smith <guy@hma.co.uk>
2021-01-19 17:42:31 +0000

    Get day19 triangles rendering

--- src/routes/Day19.svelte
+++ src/routes/Day19.svelte
@@ -1,22 +1,106 @@
 <script>
   import { onMount } from "svelte";
+  import { v4 as uuidv4 } from 'uuid';
   import Delaunator from 'delaunator';
 
   let canvas;
   let mouseX;
   let mouseY;
+  let width = 600;
+  let height = 600;
+  let delaunay;
 
   // On mouse move, create a particle with a limited lifespan. Add to array of points.
   // each Points, calculate triangles with all other points and render the triangles.
+  class Particle {
+    constructor(id, x, y, lifetime = 100) {
+      this.id = id;
+      this.x = x;
+      this.y = y;
+      this.lifetime = lifetime;
+    }
+
+    getPos() {
+      return {
+        x: this.x,
+        y: this.y,
+      }
+    }
+
+    getPosArray() {
+      return [this.x, this.y]
+    }
+
+    update() {
+      this.lifetime = this.lifetime - 1;
+      if (this.lifetime === 0) {
+        console.log('removing');
+        removeParticle(this.id);
+      }
+    }
+  }
+
+  let particles = [];
+
+  const removeParticle = id => particles = [...particles.filter( p => p.id !== id)];
+
+  const createParticle = () => withLog('creating particle', particles.push(new Particle(uuidv4(), Math.random() * width, Math.random() * height, Math.floor(Math.random() * 1000))));
+
+  const withLog = (msg, f) => {
+    console.log(msg);
+    return f;
+  }
+
+  function nextHalfedge(e) { return (e % 3 === 2) ? e - 2 : e + 1; }
+  function edgesOfTriangle(t) { return [3 * t, 3 * t + 1, 3 * t + 2]; }
+
+  function pointsOfTriangle(delaunay, t) {
+      return edgesOfTriangle(t)
+          .map(e => delaunay.triangles[e]);
+  }
+
+  function forEachTriangleEdge(points, delaunay, callback) {
+    console.log(points);
+    for (let e = 0; e < delaunay.triangles.length; e++) {
+        if (e > delaunay.halfedges[e]) {
+            const p = points[delaunay.triangles[e]];
+            const q = points[delaunay.triangles[nextHalfedge(e)]];
+            callback(e, p, q);
+        }
+    }
+  }
 
   onMount(() => {
     const ctx = canvas.getContext("2d");
 
     let frame = requestAnimationFrame(loop);
 
+    for (let i = 0; i < 20; i++) {
+      createParticle();
+    }
+
     function loop(t) {
-      frame = requestAnimationFrame(loop);
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      // frame = requestAnimationFrame(loop);
+      ctx.clearRect(0, 0, width, height);
+
+      if (t%100 == 0) createParticle();
+
+      if (particles.length === 0) return;
+
+      particles.forEach(part => {
+        // get delaunay calc, make triangles array
+        ctx.fillRect(part.x, part.y, 3, 3);
+        part.update();
+      })
+      delaunay = Delaunator.from(particles.map(p => p.getPosArray()));
+
+      forEachTriangleEdge(particles.map(p => p.getPosArray()), delaunay, (e, p, q) => {
+        console.log(delaunay);
+        ctx.beginPath();
+        ctx.moveTo(p[0], p[1]);
+        ctx.lineTo(q[0], q[1]);
+        ctx.stroke();
+      });
     }
 
     return () => {
@@ -33,4 +117,4 @@
 </style>
 
 <h1 class="page-title">Frozen Brush - Delaunay Triangles</h1>
-<canvas bind:this={canvas} width={600} height={600} />
+<canvas bind:this={canvas} width={width} height={height} />
