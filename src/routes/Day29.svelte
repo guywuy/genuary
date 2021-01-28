9c9940bc379f097c7b31e514039cc83fbcda84ab
Guy Smith <guy@hma.co.uk>
2021-01-28 18:39:09 +0000

    Change 29 to toggle between dots and circles

--- src/routes/Day29.svelte
+++ src/routes/Day29.svelte
@@ -8,47 +8,64 @@
   let canvas;
   let width = 600;
   let height = 600;
-  const numPoints = 10000;
+  const numPoints = 1000;
   const numStrips = 12;
   const stripHeight = height/numStrips;
+  let toggleCircles;
+
+  let isCircles = true;
 
   onMount(() => {
     const ctx = setupCanvas(canvas);
 
     let frame = requestAnimationFrame(draw);
 
-    const strips = [...Array(numStrips)].map((str, i) => {
-      return [...Array(numPoints)].map(_ => {
-        let posX = i % 2 === 0 ?
-        map(Math.pow(Math.random(), i + 1), 0, 1, 0, width):
-        map(Math.pow(Math.random(), i + 1), 1, 0, 0, width)
-        ; 
-        return {
-          x: posX,
-          y: (Math.random() * stripHeight) + (stripHeight * i),
-        }
-      })
-    });
+    const genStrips = () => {
+      return [...Array(numStrips)].map((str, i) => {
+        return [...Array(numPoints)].map(_ => {
+          let posX = i % 2 === 0 ?
+          map(Math.pow(Math.random(), i/3 + 1), 0, 1, 0, width):
+          map(Math.pow(Math.random(), i/3 + 1), 1, 0, 0, width)
+          ; 
+          return {
+            i,
+            x: posX,
+            y: (Math.random() * stripHeight) + (stripHeight * i),
+          }
+        })
+      });
+    }
+
+    let strips = genStrips();
 
     function draw(t) {
       // requestAnimationFrame(draw);
-      // ctx.clearRect(0, 0, width, height);
+      ctx.clearRect(0, 0, width, height);
 
       const drawPoint = s => {
-        let {x, y} = s;
+        let {x, y, i} = s;
 
         ctx.beginPath();
-        ctx.ellipse(x, y, 1, 1, 0, 0, TAU);
-        ctx.fill();
-        // ctx.stroke();
+        if (isCircles) {
+          ctx.ellipse(x, y, i + 1, i + 1, 0, 0, TAU)
+          ctx.stroke()
+        } else {
+          ctx.ellipse(x, y, 1, 1, 0, 0, TAU)
+          ctx.fill();
+        }
       }
-
-      // ctx.strokeStyle = 'white';
+      ctx.lineWidth = 0.5;
+      ctx.strokeStyle = 'black';
       ctx.fillStyle = 'black';
       strips.forEach(points => points.forEach(drawPoint));
 
     }
 
+    toggleCircles = () => {
+      isCircles = !isCircles;
+      strips = genStrips();
+      requestAnimationFrame(draw);
+    }
     return () => {
       cancelAnimationFrame(frame);
     };
@@ -69,5 +86,6 @@
 
 <h1 class="page-title">Greyscale Gradient - no lines</h1>
 <div class="canvasWrap">
-  <canvas bind:this={canvas} width={width} height={height} />
+  <p>Click on the canvas to toggle between circles and dots</p>
+  <canvas bind:this={canvas} width={width} height={height} on:click={toggleCircles}/>
 </div>
